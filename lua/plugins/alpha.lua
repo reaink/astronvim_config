0d08efaf0ea68d61a8706e54b4c01fd4573ec2ae
rea <hi@rea.ink>
2024-08-08 14:16:00 +0800

    feat: add neovim alpha config, up lock

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,6 +1,7 @@
 local new_bee_alpha_config = require "plugins.alpha_themes.new_bee"
 local rainbow_cat_alpha_config = require "plugins.alpha_themes.rainbow_cat"
 local panda_alpha_config = require "plugins.alpha_themes.panda"
+local neovim_alpha_config = require "plugins.alpha_themes.neovim"
 
 ---@type LazySpec
 return {
@@ -8,7 +9,10 @@ return {
     "goolord/alpha-nvim",
     opts = function(_, opts)
       math.randomseed(os.time())
-      local theme = ({ panda_alpha_config, rainbow_cat_alpha_config, new_bee_alpha_config })[math.random(1, 3)]
+      local theme = ({ panda_alpha_config, rainbow_cat_alpha_config, new_bee_alpha_config, neovim_alpha_config })[math.random(
+        1,
+        4
+      )]
       return theme(opts)
     end,
   },
