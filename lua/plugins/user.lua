4647d0f882868dded978be5f0af56f770f18372d
rea <hi@rea.ink>
2024-11-04 11:15:58 +0800

    fix: update avante.nvim

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -141,15 +141,8 @@ return {
     build = "make",
     -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
     opts = {
-      provider = "openai",
-      auto_suggestions_provider = "openai",
-      openai = {
-        endpoint = os.getenv "OPENAI_ENDPOINT",
-        model = "gpt-4o-mini",
-        timeout = 30000,
-        max_tokens = 4096,
-        temperature = 0,
-      },
+      provider = "copilot",
+      auto_suggestions_provider = "copilot",
       behaviour = {
         auto_suggestions = true,
       },
@@ -163,12 +156,13 @@ return {
       },
     },
     dependencies = {
+      "nvim-treesitter/nvim-treesitter",
       "stevearc/dressing.nvim",
       "nvim-lua/plenary.nvim",
       "MunifTanjim/nui.nvim",
       --- The below dependencies are optional,
       "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+      "zbirenbaum/copilot.lua", -- for providers='copilot'
       {
         -- support for image pasting
         "HakonHarnes/img-clip.nvim",
