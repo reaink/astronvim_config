82c1ca706b0cdcf0b0a8d0f07fd64319e4039309
rea <hi@rea.ink>
2024-11-28 14:17:14 +0800

    feat: add smear-cursor

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -132,4 +132,8 @@ return {
     "olrtg/nvim-emmet",
     config = function() vim.keymap.set({ "n", "v" }, "<leader>xe", require("nvim-emmet").wrap_with_abbreviation) end,
   },
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {},
+  },
 }
